13a5d3a68e8af3a8bde647c31f6441d50150c12e
rifflere <82406585+rifflere@users.noreply.github.com>
2026-01-15 07:29:19 +0000

    Button press effect lasts 1 second

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,25 @@ int main() {
     bn::core::init();
 
     bn::backdrop::set_color((bn::color(22, 5, 20)));
+    int count = 60;
 
     while(true)
     {
-        if (bn::keypad::a_pressed())
+        if (count < 60)
+        {
+            count++;
+        }
+        else if (bn::keypad::a_pressed())
         {
             bn::backdrop::set_color((bn::color(16, 0, 14)));
+            count = 0;
         } else if (bn::keypad::b_pressed())
         {
             bn::backdrop::set_color((bn::color(28, 5, 26)));
+            count = 0;
+        } else 
+        {
+            bn::backdrop::set_color((bn::color(22, 5, 20)));
         }
 
         bn::core::update();
